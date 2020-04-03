6631fb9c4946991eefc113f96c9d42fcdc594641
Heldeg <edhernandezfo@unal.edu.co>
2020-04-03 18:17:35 -0500

    Nuevas graficas

--- Practica 1/statistics.py
+++ Practica 1/statistics.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# from IPython.display import HTML, display_html, display
+
 def auto_label(rects, ax):
     # """Attach a text label above each bar in *rects*, displaying its height."""
     # https://matplotlib.org/gallery/lines_bars_and_markers/barchart.html#sphx-glr-gallery-lines-bars-and-markers-barchart-py
@@ -20,97 +22,79 @@ def auto_label(rects, ax):
 # =======================================================================
 #                               DATA
 # ========================================================================
+"""
+df_example = pd.DataFrame(np.random.rand(7 * 5, 10))
+df_example.to_csv("example.csv")"""
+
+size_group = 7
+num_of_group = 5
+decimals = 1
+
 cereal_df = pd.read_csv("example.csv")
 num = cereal_df.to_numpy()
 median = []
 deviation = []
-resolutionLabel = ["720p", "1080p", "4K"]
+kernelLabel = ["K=3", "K=5", "k=7", "K=9", "K=11", "k=13", "K=15"]
 threadsLabel = ["th=1", "th=2", "th=4", "th=8", "th=16"]
-
+# obtain media
 for val in num:
     median.append(np.median(val))
     deviation.append((np.std(val)))
-decimals = 1
-dataSec = np.around(median[0:3], decimals=decimals)
-dataTh2 = np.around(median[3:6], decimals=decimals)
-dataTh4 = np.around(median[6:9], decimals=decimals)
-dataTh8 = np.around(median[9:12], decimals=decimals)
-dataTh16 = np.around(median[12:15], decimals=decimals)
 
-speedupSec = np.around(np.divide(dataSec, dataSec), decimals=decimals)
-speedupTh2 = np.around(np.divide(dataSec, dataTh2), decimals=decimals)
-speedupTh4 = np.around(np.divide(dataSec, dataTh4), decimals=decimals)
-speedupTh8 = np.around(np.divide(dataSec, dataTh8), decimals=decimals)
-speedupTh16 = np.around(np.divide(dataSec, dataTh16), decimals=decimals)
-
-df1 = pd.DataFrame(np.array([dataSec, dataTh2, dataTh4, dataTh8, dataTh16]), index=threadsLabel,
-                   columns=resolutionLabel)
-df2 = pd.DataFrame(np.array([speedupSec, speedupTh2, speedupTh4, speedupTh8, speedupTh16]), index=threadsLabel,
-                   columns=resolutionLabel)
-speedupData = df2.to_numpy()
-
+data = []
+speedup = []
+# tabular
+for i in range(num_of_group):
+    data.append(np.around(median[i * size_group:(i * size_group) + size_group], decimals=decimals))
+    speedup.append((np.around(np.divide(data[0], data[i]), decimals=decimals)))
+data = np.array(data)
+speedup = np.array(speedup)
+df1 = pd.DataFrame(data, index=threadsLabel,
+                   columns=kernelLabel)
+df2 = pd.DataFrame(speedup, index=threadsLabel,
+                   columns=kernelLabel)
 # =======================================================================
 #                            Graphs
 # =======================================================================
 
 
-x = np.arange(len(resolutionLabel))
+x = np.arange(len(kernelLabel))
 x1 = np.arange(len(threadsLabel))
 width = 0.1
 
 fig1, ax1 = plt.subplots()
-fig2, ax2 = plt.subplots()
-fig3, ax3 = plt.subplots()
+fig3, ax2 = plt.subplots()
 
+# Graphs labels
 ax1.set_ylabel("Tiempo (s)")
 ax1.set_xlabel("Resolución")
 ax1.set_title("Tiempo de respuesta")
 ax1.set_xticks(x)
-ax1.set_xticklabels(resolutionLabel)
+ax1.set_xticklabels(kernelLabel)
 
 ax2.set_ylabel("Speedup")
-ax2.set_xlabel("Resolución")
-ax2.set_title("Speedup")
-ax2.set_xticks(x)
-ax2.set_xticklabels(resolutionLabel)
+ax2.set_xlabel("Hilos")
+ax2.set_title("Speedup por hilos")
+ax2.set_xticks(x1)
+ax2.set_xticklabels(threadsLabel)
 
-ax3.set_ylabel("Speedup")
-ax3.set_xlabel("Hilos")
-ax3.set_title("Speedup por hilos")
-ax3.set_xticks(x1)
-ax3.set_xticklabels(threadsLabel)
-
-t1 = ax1.bar(x - 2 * width, dataSec, width, label=threadsLabel[0], color='purple')
-t2 = ax1.bar(x - width, dataTh2, width, label=threadsLabel[1], color='orange')
-t3 = ax1.bar(x, dataTh4, width, label=threadsLabel[2], color='green')
-t4 = ax1.bar(x + width, dataTh8, width, label=threadsLabel[3], color='red')
-t5 = ax1.bar(x + 2 * width, dataTh16, width, label=threadsLabel[4], color='c')
-
-t6 = ax2.bar(x - width, speedupTh2, width, label=threadsLabel[1], color='orange')
-t7 = ax2.bar(x, speedupTh4, width, label=threadsLabel[2], color='green')
-t8 = ax2.bar(x + width, speedupTh8, width, label=threadsLabel[3], color='red')
-t9 = ax2.bar(x + 2 * width, speedupTh16, width, label=threadsLabel[4], color='c')
-
-t10 = ax3.plot(speedupData[:, 0], label=resolutionLabel[0])
-t11 = ax3.plot(speedupData[:, 1], label=resolutionLabel[1])
-t12 = ax3.plot(speedupData[:, 2], label=resolutionLabel[2])
+rects = []
+line = []
+# bars
+for i in range(num_of_group):
+    r = ax1.bar(x + ((i - (size_group / 4)) * width), data[i], width, label=threadsLabel[i])
+    auto_label(r, ax1)
+# lines
+for i in range(size_group):
+    ax2.plot(speedup[:, i], label=kernelLabel[i])
 
 ax1.legend()
 ax2.legend()
-ax3.legend()
-
 fig1.tight_layout()
-fig2.tight_layout()
-auto_label(t1, ax1)
-auto_label(t2, ax1)
-auto_label(t3, ax1)
-auto_label(t4, ax1)
-auto_label(t5, ax1)
-auto_label(t6, ax2)
-auto_label(t7, ax2)
-auto_label(t8, ax2)
-auto_label(t9, ax2)
+fig3.tight_layout()
 
-# display(df1)
-# display(df2)
+# display(HTML('<h2>Tiempo de respuesta</h2>'))
+# display_html(df1)
+# display(HTML('<h2>Speedup</h2>'))
+# display_html(df2)
 plt.show()
